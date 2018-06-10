9640f10ad1066b3ba446beea1a614d27a5d0569c
Matylitski Yuri <ym@tekinsoft.com>
2018-06-10 15:33:38 +0300

    added transmitter example

--- nRF51822/ble_test_board.h
+++ nRF51822/ble_test_board.h
@@ -43,27 +43,27 @@
 /* all LEDs are lit when GPIO is low */
 #define LEDS_INV_MASK  ~LEDS_MASK
 
-#define BUTTONS_NUMBER 4
+#define BUTTONS_NUMBER 2
 
 #define BUTTON_START   16
 #define BUTTON_1       16
 #define BUTTON_2       17
-#define BUTTON_3       17
-#define BUTTON_4       17
+//#define BUTTON_3       17
+//#define BUTTON_4       17
 #define BUTTON_STOP    17
 #define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
 
-#define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4 }
+#define BUTTONS_LIST { BUTTON_1, BUTTON_2 } //, BUTTON_3, BUTTON_4
 
 #define BSP_BUTTON_0   BUTTON_1
 #define BSP_BUTTON_1   BUTTON_2
-#define BSP_BUTTON_2   BUTTON_3
-#define BSP_BUTTON_3   BUTTON_4
+//#define BSP_BUTTON_2   BUTTON_3
+//#define BSP_BUTTON_3   BUTTON_4
 
 #define BSP_BUTTON_0_MASK (1<<BSP_BUTTON_0)
 #define BSP_BUTTON_1_MASK (1<<BSP_BUTTON_1)
-#define BSP_BUTTON_2_MASK (1<<BSP_BUTTON_2)
-#define BSP_BUTTON_3_MASK (1<<BSP_BUTTON_3)
+//#define BSP_BUTTON_2_MASK (1<<BSP_BUTTON_2)
+//#define BSP_BUTTON_3_MASK (1<<BSP_BUTTON_3)
 
 #define BUTTONS_ACTIVE_STATE 0
 //#define BUTTONS_MASK   0x001E0000
