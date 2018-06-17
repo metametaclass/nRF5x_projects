0b5487076a721ca01ce0bd1e0308d2a7ce7c2921
Matylitski Yury <metametaclass@gmail.com>
2018-06-17 16:21:48 +0300

    initial commit: sensor poller/transmitter

---
+++ nRF51822/sensor_tx/main.c
@@ -0,0 +1,38 @@
+//main device defines
+#include "nrf.h"
+
+#include <stdbool.h>
+
+void clock_initialization()
+{
+    /* Start 16 MHz crystal oscillator */
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_HFCLKSTART    = 1;
+
+    /* Wait for the external oscillator to start up */
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
+    {
+        __WFE();
+    }
+
+    /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
+    NRF_CLOCK->LFCLKSRC            = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_LFCLKSTART    = 1;
+
+    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
+    {
+        __WFE();
+    }
+}
+
+
+int main(void)
+{
+    clock_initialization();
+
+    while (true)
+    {
+        __WFE();
+    }
+}
